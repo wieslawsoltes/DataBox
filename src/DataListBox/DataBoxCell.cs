1d50fcad0ad5c2b6a7c96677c9d9fbcb33cf44e4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-20 23:08:17 +0200

    Add grid line to template

--- src/DataListBox/DataBoxCell.cs
+++ src/DataListBox/DataBoxCell.cs
@@ -1,15 +1,27 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using Avalonia.Controls.Templates;
-using Avalonia.Layout;
-using Avalonia.Metadata;
+using Avalonia.Controls.Shapes;
 
 namespace DataListBox
 {
     // TODO:
     public class DataBoxCell : ContentControl
     {
+        private Rectangle? _rightGridLine;
+
         internal double MeasuredWidth;
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+            
+            _rightGridLine = e.NameScope.Find<Rectangle>("PART_RightGridLine");
+
+            if (_rightGridLine is { })
+            {
+                // TODO:
+                _rightGridLine.IsVisible = false;
+            }
+        }
     }
 }
