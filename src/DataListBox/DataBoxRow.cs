f1bd2c08d96e08d41ed83a24e3e9aab0c63fddbc
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-21 12:50:20 +0200

    Add grid line field

--- src/DataListBox/DataBoxRow.cs
+++ src/DataListBox/DataBoxRow.cs
@@ -1,6 +1,7 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
 using Avalonia.Styling;
 using DataListBox.Primitives;
 
@@ -8,6 +9,8 @@ namespace DataListBox
 {
     public class DataBoxRow : ListBoxItem, IStyleable
     {
+        private Rectangle? _bottomGridLine;
+
         Type IStyleable.StyleKey => typeof(DataBoxRow);
 
         internal DataBoxCellsPresenter? CellsPresenter { get; set; }
@@ -15,6 +18,8 @@ namespace DataListBox
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
+
+            _bottomGridLine = e.NameScope.Find<Rectangle>("PART_BottomGridLine");
 
             CellsPresenter = e.NameScope.Find<DataBoxCellsPresenter>("PART_CellsPresenter");
         }
